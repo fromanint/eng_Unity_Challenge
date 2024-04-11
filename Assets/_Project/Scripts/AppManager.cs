02107795198d50bac6addb45ab473987f34893a4
fromanint <fromaninteractive@gmail.com>
2024-04-10 23:32:13 -0600

    Select Character Complete
    Done:
    
    -Script to get the selected Character
    -AppManager Script
    -Create Character prefabs
    -Start Creating Maze Scene
    
    Next steps:
    
    -Create Levels
    -Change animations for characters (Animation Controller)
    -Script to move around the Level (Maze)
    -Script to get the collectibles
    -Script to find the Main Object
    -Script for time count
    -Script to BestScore
    - Script to get the tools
    - Minimap & Open Minimap

---
+++ Assets/_Project/Scripts/AppManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    public GameObject selectedCharacter;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        AppManager[] appManagers = FindObjectsOfType<AppManager>();
+        if(appManagers.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    
+}
--- Assets/_Project/Scripts/CharacterSelection.cs
+++ Assets/_Project/Scripts/CharacterSelection.cs
@@ -3,18 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(ChangeScene))]
 public class CharacterSelection : MonoBehaviour
 {
-    [SerializeField] List<Button> characterList;
+
+    [Header("PLEASE CHOOSE THE CHARACTERS IN THE CORRECT ORDER")]
+    [SerializeField] List<GameObject> characterList;
+
+    [Header("NEXT SCENE NAME")]
+    [SerializeField] string newSceneName;
+
+    [Header("GET FIRST BUTTON")]
+    [SerializeField] Button firstSelected;
+
+    [Header("BACKGROUND COLORS FOR SELECTION")]
+    [SerializeField] Color selectedColor;
+    [SerializeField] Color unSelectColor;
+    int previousSelected;
+    
+
+    
     // Start is called before the first frame update
     void Start()
     {
+        firstSelected.Select();
+        previousSelected = 0;
+
+    }
+    public void ChangeSelected(int index)
+    {
+        if (index == previousSelected)
+        {
+            AppManager appManager = FindObjectOfType<AppManager>();
+            appManager.selectedCharacter = characterList[index];
+            GetComponent<ChangeScene>().ChangeSceneOnClick(newSceneName);
+        }
+        else
+        {
+            previousSelected = index;
+        }
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    
+
 }
